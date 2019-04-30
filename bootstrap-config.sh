f3d6b972f545fa776376b3221f523a2e24b0e7ee
Safronov Pavel <pv.safronov@gmail.com>
2019-04-30 18:58:27 +0300

    Bump ansible version

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -1,5 +1,5 @@
 BOOTSTRAP_DIR="${HOME}/.local/share/bootstrap"
 ANSIBLE_VENV_DIR="${BOOTSTRAP_DIR}/ansible-venv"
 ANSIBLE_REPO_DIR="${BOOTSTRAP_DIR}/ansible-repo"
-ANSIBLE_VERSION="2.4.0.0"
+ANSIBLE_VERSION="2.7.10"
 
