8d68e93565eaccc7aecdb3c607c845464c0ecebf
Pavel Safronov <pv.safronov@gmail.com>
2023-03-20 20:52:29 +0000

    Reset pythonpath if set outside

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -2,4 +2,4 @@ BOOTSTRAP_DIR="${HOME}/.local/share/bootstrap"
 ANSIBLE_VENV_DIR="${BOOTSTRAP_DIR}/ansible-venv"
 ANSIBLE_REPO_DIR="${BOOTSTRAP_DIR}/ansible-repo"
 ANSIBLE_VERSION="2.10.7"
-
+export PYTHONPATH=""
