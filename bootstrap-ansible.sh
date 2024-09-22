856c0856f12c7fd50643a12a44bb5401174890a2
Pavel Safronov <pv.safronov@gmail.com>
2024-09-22 11:08:44 +0000

    cargo is not required for ansible

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -9,7 +9,7 @@ then
 
     sudo apt-get -y install build-essential libssl-dev libffi-dev \
 			       python3-pip python3-virtualenv git \
-			       python3-dev cargo pkg-config acl
+			       python3-dev pkg-config acl
 fi
 
 if which yum 2>&1 >/dev/null
